34394e61e73f89918450ab7280d510668bfc686e
Cristian Poley <cristianpoleygonzalez@gmail.com>
2021-06-20 18:28:03 +0200

    feat: finish style desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import '../styles/header-styles.css'
 import Logo from '../assets/Logo.jsx'
+import arrow from '../assets/icon-arrow-down.svg'
 
 export default function Header() {
   return (
     <section className="header">
-        <Logo className="header-logo" eight={20} fill={'#fff'} />
-        <div className="header-buttons">
-            <a href="#about">
-                About
-            </a>
-            <a href="#services">
-                Services
-            </a>
-            <a href="#projects">
-                Projects
-            </a>
-            <a href="#contact">
-                Contact
-            </a>
+        <div className="header-content">
+            <Logo className="header-logo" height={30} fill={'#fff'} />
+            <div className="header-buttons">
+                <a href="#about">
+                    About
+                </a>
+                <a href="#services">
+                    Services
+                </a>
+                <a href="#projects">
+                    Projects
+                </a>
+                <a href="#contact" className="contact">
+                    Contact
+                </a>
+            </div>
         </div>
         <h1>
             WE ARE CREATIVES
+            <img src={arrow} />
         </h1>
     </section>
   )
